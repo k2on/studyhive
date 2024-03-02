aa9123d44be0537b52587faba135b7aaa2313376
YashPatelSC <101068968+YashPatelSC@users.noreply.github.com>
2024-03-02 13:51:43 -0500

    add homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@ import { unstable_noStore as noStore } from "next/cache";
 import Link from "next/link";
 
 import { CreatePost } from "~/app/_components/create-post";
+import { Button } from "~/components/ui/button";
+import { Card, CardDescription, CardHeader, CardTitle } from "~/components/ui/card";
+import { Input } from "~/components/ui/input";
 import { getServerAuthSession } from "~/server/auth";
 import { api } from "~/trpc/server";
 
@@ -12,8 +15,31 @@ export default async function Home() {
 
   return (
     <main className="">
-      <div className="max-w-xl mx-auto">
-        blahblah
+      <div className="max-w-xl mx-auto pt-4">
+        <div className="flex">
+           <Input placeholder="Search for a class"/>
+           <Button> New</Button>
+        </div>
+        <div className="pt-8 flex flex-col space-y-4">
+          <Card>
+            <CardHeader>
+              <CardTitle>Class Name</CardTitle>
+              <CardDescription>Teacher Name</CardDescription>
+            </CardHeader>
+          </Card>
+          <Card>
+            <CardHeader>
+              <CardTitle>Class Name</CardTitle>
+              <CardDescription>Teacher Name</CardDescription>
+            </CardHeader>
+          </Card>
+          <Card>
+            <CardHeader>
+              <CardTitle>Class Name</CardTitle>
+              <CardDescription>Teacher Name</CardDescription>
+            </CardHeader>
+          </Card>
+        </div>
       </div>
     </main>
   );
