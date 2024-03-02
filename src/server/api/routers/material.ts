cab153d85c0a62cfa16135dfacfbe7c38abe91fa
anabasis <anabasis@noreply.codeberg.org>
2024-03-02 17:32:20 -0500

    fix: move materialRouter from incorrect file

--- src/server/api/routers/material.ts
+++ src/server/api/routers/material.ts
@@ -3,20 +3,27 @@ import { z } from "zod";
 import {
   createTRPCRouter,
   protectedProcedure,
-  publicProcedure,
 } from "~/server/api/trpc";
 import { courseMaterials } from "~/server/db/schema";
-import {v4} from "uuid"
+import { eq } from "drizzle-orm";
 
 export const materialsRouter = createTRPCRouter({
-
   create: protectedProcedure
-    .input(z.object({ name: z.string().min(1), term: z.string().min(1), id: z.string().min(1),courseId: z.string().min(1) }))
+    .input(z.object({ name: z.string().min(1), term: z.string().min(1), id: z.string().min(1), courseID: z.string().min(1) }))
     .mutation(async ({ ctx, input }) => {
-      await ctx.db.insert(courseMaterials).values({
+      return await ctx.db.insert(courseMaterials).values({ 
         id: input.id,
         name: input.name,
-        term: ctx.session.user.id,
+        term: input.term,
+        courseID: input.courseID,
       });
     }),
+
+  getForCourse: protectedProcedure
+    .input(z.string())
+    .query(async ({ ctx, input }) => {
+      return await ctx.db.query.courseMaterials.findMany({
+        where: eq(courseMaterials.courseID, input),
+      });
+  }),
 });
