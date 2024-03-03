b6e7607a79422b34bc463ee2be97aea6c8d9fc3c
shihabdelr <sabdelr@clemson.edu>
2024-03-03 01:35:44 -0500

    Materials back button functionality

--- src/app/[course]/[materials]/page.tsx
+++ src/app/[course]/[materials]/page.tsx
@@ -2,7 +2,9 @@ import { eq } from "drizzle-orm";
 import { db } from "~/server/db";
 import { courseMaterials, courses } from "~/server/db/schema";
 import { AddQuestion, Questions } from "./questions";
-
+import Link from "next/link";
+import { Button } from "~/components/ui/button";
+import {  ChevronLeft } from "lucide-react"
 export interface Props{
     params: {
         course: string,
@@ -21,12 +23,22 @@ export default async function Material({ params }: Props) {
     if (!material) return "Material not found";
 
     return <div className="max-w-xl mx-auto">
+        
+        <div>    
+        <Link href = {"/" + params.course}>
+            <Button variant="secondary" className="">
+                <ChevronLeft className="justify-left mr-2 h-4 w-4" />
+            {course.name} - {course.instructorName}
+            </Button>
+            </Link>
+        </div>
         <div className="flex space-x-2 justify-between">
            <h1 className="text-3xl font-bold">{material.name}</h1>
         </div>
         <div>
-            {material.term} - {course.name} - {course.instructorName}
+        {material.term}
         </div>
+        <br/>
         <Questions />
         <AddQuestion />
     </div>
